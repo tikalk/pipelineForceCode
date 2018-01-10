599e7eef197bddf59c1b10daf5ee165e9c57d232
Yoram Michaeli <yorammi@tikalk.com>
2018-01-10 06:04:56 +0200

    Add python tests

--- python/test/test_set1.py
+++ python/test/test_set1.py
@@ -1,12 +1,12 @@
 import time
 
 def test_text_after_sleep():
-    time.sleep(10)
+    time.sleep(1)
     assert 'test' == 'test'
 
 def test_text_before_sleep():
     assert 'test' == 'test'
-    time.sleep(10)
+    time.sleep(1)
 
 def test_failed_assertion():
     assert 'test1' == 'test2'
--- python/test/test_set2.py
+++ python/test/test_set2.py
@@ -1,10 +1,10 @@
 import time
 
 def test_number_after_sleep():
-    time.sleep(5)
+    time.sleep(1)
     assert 5 == 5
 
 def test_number_before_sleep():
     assert 5 == 5
-    time.sleep(5)
+    time.sleep(1)
 
