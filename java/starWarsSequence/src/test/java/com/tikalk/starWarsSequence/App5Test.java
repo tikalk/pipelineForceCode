2fa5b01e1894bb08d9d1486262684a663c843799
Yoram Michaeli <yorammi@tikalk.com>
2018-02-19 11:21:24 +0200

    starWarsSequence

---
+++ java/starWarsSequence/src/test/java/com/tikalk/starWarsSequence/App5Test.java
@@ -0,0 +1,39 @@
+package com.tikalk.starWarsSequence;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class App5Test
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public App5Test( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( App5Test.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        System.out.println("*** App5Test ***");
+        assertTrue( false );
+    }
+}
